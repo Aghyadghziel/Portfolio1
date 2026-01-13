dd7d83e4cfed9a90d51e39611156d8dbc2eb933c
aghyadghziel <aghyadghziel@gmail.com>
2026-01-13 15:19:09 +0400

    update

--- src/components/work/Projects.tsx
+++ src/components/work/Projects.tsx
@@ -16,7 +16,7 @@ export function Projects({ range, exclude }: ProjectsProps) {
   }
 
   const sortedProjects = allProjects.sort((a, b) => {
-    return new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime();
+    return new Date(a.metadata.publishedAt).getTime() - new Date(b.metadata.publishedAt).getTime();
   });
 
   const displayedProjects = range
